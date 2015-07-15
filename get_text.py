b8527d2ea619aa317709f17ebfafe442e93e619a
Peter Hastings <peterh@cdm.depaul.edu>
2015-07-15 16:46:14 -0600

    Adding speech input, with minor grammar mod.

---
+++ get_text.py
@@ -0,0 +1,57 @@
+# NOTE: this requires PyAudio because it uses the Microphone class
+import os
+import speech_recognition as sr
+
+def say (text):
+    os.system("say -v Vicki " + "\"" + text + "\"")
+
+def get_text():
+    r = sr.Recognizer()
+    good_recording = False
+    text = ""
+    while not good_recording:
+        print "Say your statement now"
+        say("Say your statement now")
+        # use the default microphone as the audio source
+        with sr.Microphone() as source:
+            # listen for the first phrase and extract it into audio data
+            audio = r.listen(source)  
+        try:
+            text = str(r.recognize(audio))
+            # recognize speech using Google Speech Recognition
+            print("You said: " + text)  
+            say( "You said: " + text)
+            say("Is this correct?")
+            response = raw_input('Is this correct? Type y or n or q (to quit): ')
+            if response == "y":
+                good_recording = True
+            elif response == "q":
+                exit()
+        except LookupError:  # speech is unintelligible
+            text = "I did not understand"
+            print(text)
+            say(text)
+    return text
+
+
+# subjects = ["john", "bob", "tom", "mary", "susan"]
+# places = ["kitchen", "lab", "office", "bathroom", "attic"]
+# objects = ["ball", "bat", "book", "bottle", "hat"]
+# questions = ["who", "what", "where"]
+
+# memory=[]
+
+def getSentence(words):
+    text=get_text();
+    print "initial text: " + text
+    text_list = text.lower().split()
+    final_list=[]
+    for i in range(len(text_list)):
+        word=text_list[i];
+        if (word in words):
+            final_list.append(text_list[i])
+    if (final_list[0][0] == 'w'):
+        final_list.append('?')
+    else:
+        final_list.append('.')
+    return final_list
